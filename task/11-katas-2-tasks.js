b146c16bd2116a7166a17d2bb500db301fd05162
PsqNew <balahonov.dmitry@gmail.com>
2019-04-14 20:58:31 +0300

    add: add solution parseBankAccount

--- task/11-katas-2-tasks.js
+++ task/11-katas-2-tasks.js
@@ -34,7 +34,20 @@
  *
  */
 function parseBankAccount(bankAccount) {
-    throw new Error('Not implemented');
+    const config = {
+        ' _ | ||_|': 0,
+        '     |  |': 1,
+        ' _  _||_ ': 2,
+        ' _  _| _|': 3,
+        '   |_|  |': 4,
+        ' _ |_  _|': 5,
+        ' _ |_ |_|': 6,
+        ' _   |  |': 7,
+        ' _ |_||_|': 8,
+        ' _ |_| _|': 9
+    };
+    let res = bankAccount.split('\n').slice(0, 3).map(item => item.match(/.../g));
+    return +res[0].map((elem, i) => config[elem + res[1][i] + res[2][i]]).join("");
 }
 
 
